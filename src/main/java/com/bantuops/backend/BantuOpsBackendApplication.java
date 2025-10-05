f7eff29a322b4e10a8825e7cc549835d79e798b3
paul.mika <paul.mika@fahpo.com>
2025-10-05 18:57:50 +0000

    first commit

---
+++ src/main/java/com/bantuops/backend/BantuOpsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bantuops.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BantuOpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BantuOpsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bantuops/backend/BantuOpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bantuops.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BantuOpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
